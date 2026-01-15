995acac041758371c0426648ba9137891489d31e
Mateus Derossi <mateus.derossi@zgsolucoes.com.br>
2026-01-14 22:59:37 -0300

    add connection to amqp broker

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,13 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
 }
 
 main().catch((err) => {
+  amqp.connect("amqp://guest:guest@localhost:5672/");
   console.error("Fatal error:", err);
   process.exit(1);
+}).then(() => {
+  console.log("Peril server started successfully.");
 });
