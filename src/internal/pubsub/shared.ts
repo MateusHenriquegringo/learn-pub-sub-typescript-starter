d069d8c9183dc617e76707ea0427f597af485ea0
mateus.derossi <henriquemateus2502@gmail.com>
2026-01-24 18:34:03 -0300

    add subscribe function

--- src/internal/pubsub/shared.ts
+++ src/internal/pubsub/shared.ts
@@ -1,4 +1,5 @@
 import type { ConfirmChannel, Channel, ChannelModel, Replies } from "amqplib";
+export type SimpleQueueType = { durable?: boolean, transient: boolean };
 
 export function publishJSON<T>(ch: ConfirmChannel, exchange: string, routingKey: string, value: T): Promise<void> {
 
@@ -17,7 +18,7 @@ export async function declareAndBind(
 	exchange: string,
 	queueName: string,
 	key: string,
-	queueType: { durable?: boolean, transient: boolean },
+	queueType: SimpleQueueType,
 ): Promise<[Channel, Replies.AssertQueue]> {
 
 	const channel = await conn.createConfirmChannel()
@@ -32,4 +33,26 @@ export async function declareAndBind(
 
 	return [channel, assertQueue]
 
+};
+
+
+export async function subscribeJSON<T>(
+		conn: ChannelModel,
+		exchange: string,
+		queueName: string,
+		key: string,
+		queueType: SimpleQueueType,
+		handler: (data: T) => void,
+): Promise<void> {
+
+	let [channel, queue] = await declareAndBind(conn, exchange, queueName, key, queueType)
+
+	await channel.consume(queue.queue, (msg) => {
+		if (!msg) return;
+
+		const parsed = JSON.parse(msg.content.toString()) as T;
+		handler(parsed)
+
+		channel.ack(msg)
+	})
 };
